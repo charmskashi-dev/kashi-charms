d7fe024fb94a2325b568f589ea682801704095b1
24-ANONYMOUS <ms.harshita.vats@gmail.com>
2026-05-07 19:22:12 +0530

    Removed auth restriction from cart and wishlist

--- app/(client)/wishlist/page.tsx
+++ app/(client)/wishlist/page.tsx
@@ -1,19 +1,8 @@
-import NoAccess from "@/components/NoAccess";
 import WishListProducts from "@/components/WishlistProducts";
-import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
-const WishListPage = async () => {
-  const user = await currentUser();
-  return (
-    <>
-      {user ? (
-        <WishListProducts />
-      ) : (
-        <NoAccess details="Log in to view your wishlist items. Don’t miss out on your cart products to make the payment!" />
-      )}
-    </>
-  );
+const WishListPage = () => {
+  return <WishListProducts />;
 };
 
 export default WishListPage;
