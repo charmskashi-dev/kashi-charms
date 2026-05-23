66e567b0c09b84287a803c553d93841bf7b206dd
24-ANONYMOUS <ms.harshita.vats@gmail.com>
2026-05-23 15:25:21 +0530

    added footer policy links

--- constants/data.ts
+++ constants/data.ts
@@ -1,21 +1,35 @@
+export const headerData = [
+  { title: "Home", href: "/" },
+  { title: "Shop", href: "/shop" },
+  { title: "Blog", href: "/blog" },
+  { title: "Hot Deal", href: "/deal" },
+  //   { title: "Contact", href: "/contact" },
+];
 export const quickLinksData = [
-  { title: "About Us", href: "/about" },
-
-  { title: "Contact Us", href: "/contact" },
-
-  { title: "My Orders", href: "/orders" },
-
-  { title: "Terms & Conditions", href: "/terms" },
-
-  { title: "Privacy Policy", href: "/privacy" },
-
+  { title: "About us", href: "/about" },
+  { title: "Contact us", href: "/contact" },
+  { title: "Track Orders", href: "/orders" },
   { title: "Shipping Policy", href: "/shipping-policy" },
-
   { title: "Return & Refund Policy", href: "/return-policy" },
-
-  { title: "Cancellation Policy", href: "/cancellation-policy" },
-
+  { title: "Terms & Conditions", href: "/terms" },
+  { title: "Privacy Policy", href: "/privacy" },
   { title: "FAQs", href: "/faqs" },
-
   { title: "Help", href: "/help" },
+];
+export const categoriesData = [
+  { title: "Best Sellers", href: "best-sellers" },
+  { title: "New Arrivals", href: "new-arrivals" },
+  { title: "Earrings", href: "earrings" },
+  { title: "Necklaces", href: "necklaces" },
+  { title: "Rings", href: "rings" },
+  { title: "Bracelets", href: "bracelets" },
+  { title: "Resin Jewellery", href: "resin-jewellery" },
+  { title: "Festive Hampers", href: "festive-hampers" },
+  { title: "Combos", href: "combos" },
+];
+export const productType = [
+  { title: "Necklaces", value: "necklaces" },
+  { title: "Earrings", value: "earrings" },
+  { title: "Rings", value: "rings" },
+  { title: "Bracelets", value: "bracelets" },
 ];
