01eb8e0883e3f7123a10a6d8d84eff3746e5f55b
24-ANONYMOUS <ms.harshita.vats@gmail.com>
2026-07-11 22:55:18 +0530

    Google Tag Added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 import { SanityLive } from "@/sanity/lib/live";
 
@@ -10,6 +11,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-MNQZ59GNFS"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MNQZ59GNFS');
+            `}
+          </Script>
+        </head>
         <body className="font-poppins antialiased">
           {children}
 
